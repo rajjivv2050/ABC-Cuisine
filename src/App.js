291f3e8053168a7cf55ae825dbff2df9d9fcc97a
rajjivv2050 <rajjivv2050@gmail.com>
2020-12-10 17:53:07 +0530

    1st commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import DebitCardPay from './components/DebitCardPay';
+
+function App() {
+  return (
+    
+    <DebitCardPay></DebitCardPay>
+  );
+}
+
+export default App;
---
+++ src/components/DebitCardPay.js
@@ -0,0 +1,15 @@
+import React,{Component}  from 'react';
+
+class DebitCardPay extends Component{
+    render()
+    {
+        return (
+            <div>
+                <label><h1>Please pay Rs 1000</h1></label>
+                <button>Cash on delivery</button>
+            </div>
+        )
+    }
+}
+
+export default DebitCardPay
